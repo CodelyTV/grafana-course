113142a0d2870bf160d21c67027ffc5124f3091c
Joan López de la Franca Beltran <joanjan14@gmail.com>
2023-08-03 15:32:14 +0200

    Replace files copy in favor of Git submodule

--- 3-fullstack-e2e-web-shop-o11y/web-shop-o11y-demo/simulate-user-traffic.sh
+++
@@ -1,1 +0,0 @@
-docker run --network web-shop-o11y-demo_web-shop --rm -i grafana/k6  run -e HOSTNAME=${PUBLIC_APP_URL:=web-shop} - <shop-simulator/webshop-protocol-demo.js
--- 3-fullstack-e2e-web-shop-o11y/web-shop-o11y-demo/xk6-browser/Readme.md
+++
@@ -1,5 +0,0 @@
-# Docker image build for k6 with xk6-browser
-
-```
-docker build -t xk6-browser:v0.7.0 .
-```
